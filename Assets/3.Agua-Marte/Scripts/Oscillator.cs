ec98b09b300798225c3c7766e2079091231742fe
Rafael Ramírez <rafael.ram.lop@gmail.com>
2021-09-15 16:43:44 -0500

    recorder
    Se agrega botón para compartir

--- Assets/3.Agua-Marte/Scripts/Oscillator.cs
+++ Assets/3.Agua-Marte/Scripts/Oscillator.cs
@@ -1,51 +1,51 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 
 public class Oscillator : MonoBehaviour
 {
+    [SerializeField, Tooltip("Objeto al que se tomará como centro de la translación")] 
     public Transform rotationCenter;
-    public float rotationRadius = 0.2f, angularSpeed = 1f;
-
-    float posX = 0, posY = 0, posZ, angle = 0;
-    public float alturaX = 0, alturaY = 0, alturaZ = 0;
-    //public TextMeshProUGUI valor;
-    bool completada = false;
+    [SerializeField, Tooltip("Altura a la que el satélite va a rotar")]
+    public float rotationRadius = 0.2f;
+    [SerializeField, Tooltip("")]
+    public float angularSpeed = 1f;
+    [SerializeField, Tooltip("Canvas en el que se va a desplegar la información")] 
     public GameObject canvasAgua;
+
+    private float posX = 0, posY = 0, posZ, angle = 0;
+    private bool completada = false;
 
     void Update()
     {
-        //Mientras no se hayan dado 3 vueltas sigue moviendose
+        //Mientras no se hayan dado 3 vueltas el satélite sigue moviéndose
         if (!completada)
         {
             posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
             //posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
             posZ = rotationCenter.position.z + Mathf.Sin(angle) * rotationRadius;
 
-            transform.localPosition = new Vector3(posX + alturaX, posY + alturaY, posZ + alturaZ);
+            transform.localPosition = new Vector3(posX, posY, posZ);
             angle = angle + Time.deltaTime * angularSpeed;
         }
 
-        //Si el satelite ya dio 3 vueltas (aprox. 7 grados por vuelta) mostramos la imagen
+        //Si el satelite ya dio 3 vueltas (aprox. 7 grados por vuelta) mostramos la imagen del agua
         if (angle >=21f)
         {
-            //if (angle >= 360f)
             angle = 0f;
             completada = true;
-            canvasAgua.gameObject.SetActive(true);
+            canvasAgua.SetActive(true);
             StartCoroutine(muestraAgua());
-            
         }
-        //valor.SetText(angle.ToString());
     }
-   
-    //FUncion que detiene la imagen del agua durante 8 segundos
-    IEnumerator muestraAgua()
+
+    /// <summary>
+    /// Funcion que detiene la imagen del agua durante 10 segundos
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator muestraAgua()
     {
-        yield return new WaitForSeconds(8);
-        canvasAgua.gameObject.SetActive(false);
+        yield return new WaitForSeconds(10);
+        canvasAgua.SetActive(false);
         completada = false;
     }
 }
