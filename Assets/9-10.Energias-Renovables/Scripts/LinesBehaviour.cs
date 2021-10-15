5cc1ffa2421ce36e0dd2208cdeceff83787775a1
FeSanz <felipe.antonio.condor@gmail.com>
2021-10-15 00:07:33 -0500

    Lineas de energia
    Lineas de energia completas

---
+++ Assets/9-10.Energias-Renovables/Scripts/LinesBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinesBehaviour : MonoBehaviour
+{
+    [SerializeField, Tooltip("Vertices de referencia para dibujar linea")] private List<Transform> vertex = new List<Transform>();
+    private LineRenderer line;
+
+
+    void Start()
+    {
+        DrawLine();
+    }
+    public void DrawLine()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.startWidth = 0.5f;
+        line.endWidth = 0.5f;
+        line.widthMultiplier = 3.0f;
+        //line.startColor = Color.red;
+        //line.endColor = Color.red;
+        line.positionCount = vertex.Count;
+
+        for(int i = 0; i < vertex.Count; i++)
+        {
+            line.SetPosition(i, vertex[i].localPosition);
+        }
+    }
+
+}
