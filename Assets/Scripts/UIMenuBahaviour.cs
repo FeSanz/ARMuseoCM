9db9a7195906fe98bca6c44261ffbc94aec78a5f
FeSanz <felipe.antonio.condor@gmail.com>
2021-10-19 00:21:13 -0500

    Texturas de ER recuperadas y optimizadas
    Texturas completas, se redujo de 452 MB a 16MB en sistema solar y de 305MB a 6MB en energías renovables (ER)

--- Assets/Scripts/UIMenuBahaviour.cs
+++ Assets/Scripts/UIMenuBahaviour.cs
@@ -1,12 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+#if PLATFORM_ANDROID
+    using UnityEngine.Android;
+#endif
 
 public class UIMenuBahaviour : MonoBehaviour
 {
     [SerializeField, Tooltip("GridLayout del menú de opciones")]
     private GridLayoutGroup _gridLayoutGroup;
+
+    [SerializeField, Tooltip("Animacion de alerta de permisos")]
+    private Animator AlertPermissionAnim;
+    
+    void Start ()
+    {
+        #if PLATFORM_ANDROID
+            StartCoroutine(AskForPermissions());
+        #endif
+    }
 
     void Update()
     {
@@ -50,5 +65,66 @@ public class UIMenuBahaviour : MonoBehaviour
     {
         _gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;  //Layout por columnas
         _gridLayoutGroup.constraintCount = count; //Numero de columnas
+    }
+    
+    private IEnumerator AskForPermissions()
+    {
+#if UNITY_ANDROID
+        List<bool> permissions = new List<bool>() { false, false, false, false };
+        List<bool> permissionsAsked = new List<bool>() { false, false, false, false };
+        List<Action> actions = new List<Action>()
+        {
+            new Action(() => {
+                permissions[0] = Permission.HasUserAuthorizedPermission(Permission.Microphone);
+                if (!permissions[0] && !permissionsAsked[0])
+                {
+                    Permission.RequestUserPermission(Permission.Microphone);
+                    permissionsAsked[0] = true;
+                    return;
+                }
+            }),
+            new Action(() => {
+                permissions[1] = Permission.HasUserAuthorizedPermission(Permission.Camera);
+                if (!permissions[1] && !permissionsAsked[1])
+                {
+                    Permission.RequestUserPermission(Permission.Camera);
+                    permissionsAsked[1] = true;
+                    return;
+                }
+            }),
+            new Action(() => {
+                permissions[2] = Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite);
+                if (!permissions[2] && !permissionsAsked[2])
+                {
+                    Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+                    permissionsAsked[2] = true;
+                    return;
+                }
+            }),
+            new Action(() => {
+                permissions[3] = Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead);
+                if (!permissions[3] && !permissionsAsked[3])
+                {
+                    Permission.RequestUserPermission(Permission.ExternalStorageRead);
+                    permissionsAsked[3] = true;
+                    return;
+                }
+            })
+        };
+        for(int i = 0; i < permissionsAsked.Count; )
+        {
+            actions[i].Invoke();
+            if(permissions[i])
+            {
+                ++i;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+#endif
+    }
+
+    public void ExitOfApplication()
+    {
+        Application.Quit();
     }
 }
