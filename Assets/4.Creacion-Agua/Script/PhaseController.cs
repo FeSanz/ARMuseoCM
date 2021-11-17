4bdb277e2c2e48acdd7edb4c967c544d097e4e4a
FeSanz <felipe.antonio.condor@gmail.com>
2021-11-17 11:44:56 -0600

    Creacion del agua terminado
    Se realizaron las validaciones restantes

--- Assets/4.Creacion-Agua/Script/PhaseController.cs
+++ Assets/4.Creacion-Agua/Script/PhaseController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PhaseController : MonoBehaviour
 {
@@ -15,18 +16,27 @@ public class PhaseController : MonoBehaviour
     [SerializeField, Tooltip("Aureola de fuego")]
     private GameObject Halo;
     
+    [SerializeField, Tooltip("Aureola de fuego")]
+    private GameObject RestartButton;
+    
     [SerializeField, Tooltip("Velocidad de aparicion de cada shader en transisición")]
     private float speed = 0.5f;
 
     [SerializeField, Tooltip("Título de la etapa")]
     private TextMeshProUGUI titulo;
     
-    [SerializeField, Tooltip("Descrpcion de la etapa")]
+    [SerializeField, Tooltip("Descripcion de la etapa")]
     private TextMeshProUGUI descripcion;
+    
+    [SerializeField, Tooltip("Notificación tipo SnackBar")]
+    private GameObject SnackBar;
 
     private float _stageValue = 1;
     private Material _materialEarth;
     private int _stage = -1;
+
+    private bool _targetFound = false;
+    private bool _backScene = false;
 
     void Start()
     {
@@ -35,6 +45,15 @@ public class PhaseController : MonoBehaviour
     
     private void Update()
     {
+        if (AudioSettings.Mobile.muteState && _targetFound)
+        {
+            SnackBar.SetActive(true); 
+        }
+        else
+        {
+            SnackBar.SetActive(false);
+        } 
+        
         switch (_stage)
         {
             case 1:
@@ -42,11 +61,6 @@ public class PhaseController : MonoBehaviour
                 {
                     _stageValue -= Time.deltaTime * speed;
                     _materialEarth.SetFloat("FloatEarth", _stageValue);
-                }
-
-                if (_stage == 0)
-                {
-                    
                 }
                 break;
             case 2:
@@ -83,6 +97,7 @@ public class PhaseController : MonoBehaviour
         _stageValue = 1;
         _stage = 1;
         gameObject.GetComponent<LookAtCamera>().enabled = true;
+        yield return new WaitForSeconds (1);
         Halo.SetActive(true);
         titulo.text = "ETAPA 1";
         descripcion.text = "La temperatura de la tierra primitiva era demasiado alta, de tal manera que con ayuda del impacto de " +
@@ -119,6 +134,8 @@ public class PhaseController : MonoBehaviour
         AudioStage[4].Play();
         titulo.text = "ETAPA 4";
         descripcion.text = "El agua en la tierra en la actualidad es necesaria para la formación de otras moléculas que son responsables del origen de la vida en la Tierra";
+        yield return new WaitForSeconds (14);
+        RestartButton.SetActive(true);
     }
 
     IEnumerator ActiveMeteorite()
@@ -134,6 +151,54 @@ public class PhaseController : MonoBehaviour
     }
     public void StartStage()
     {
+        _targetFound = true;
         StartCoroutine(StageController());
+    }
+
+    public void MuteAudio()
+    {
+        foreach (AudioSource audio in AudioStage)
+        {
+            audio.mute = true;
+        }
+    }
+    
+    public void SpeakAudio()
+    {
+        foreach (AudioSource audio in AudioStage)
+        {
+            audio.mute = false;
+        }
+    }
+
+    public void ValidateBackScene()
+    {
+        _backScene = true;
+        _targetFound = false;
+        Debug.Log("MenuScene");
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void TargetLostAfterFound()
+    {
+        if (_targetFound && !_backScene)
+        {
+            _targetFound = false;
+            Debug.Log("TargetLostAfterFound");
+            SceneManager.LoadScene("CreacionAguaScene");
+        }
+    }
+
+    public void ReloadScene()
+    {
+        _backScene = false;
+        _targetFound = false;
+        Debug.Log("Reload");
+        SceneManager.LoadScene("CreacionAguaScene");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("Disable");
     }
 }
