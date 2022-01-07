f77da83f2c8596231d7fe30dae40ee93df060844
FeSanz <felipe.antonio.condor@gmail.com>
2022-01-07 15:01:33 -0600

    Audios parte 1
    En proceso

---
+++ Assets/Scripts/SoundValidate.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoundValidate : MonoBehaviour
+{
+    [SerializeField, Tooltip("Notificacion para aumentar volumen tipo SnackBar")]
+    private GameObject SnackBar;
+
+    [SerializeField, Tooltip("Audios de cada secuencia")]
+    private AudioSource[] AudioStage;
+    void Update()
+    {
+        if (AudioSettings.Mobile.muteState)
+        {
+            SnackBar.SetActive(true); 
+        }
+        else
+        {
+            SnackBar.SetActive(false);
+        } 
+    }
+    
+    public void MuteAudio()
+    {
+        foreach (AudioSource audio in AudioStage)
+        {
+            audio.mute = true;
+        }
+    }
+    
+    public void SpeakAudio()
+    {
+        foreach (AudioSource audio in AudioStage)
+        {
+            audio.mute = false;
+        }
+    }
+   
+}
